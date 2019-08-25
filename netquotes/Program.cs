234a29e463aacf6f66a808e83e696512bc103a40
Diego López <diegolopezperez91@gmail.com>
2019-08-25 11:58:58 +0200

    Reorder code

--- netquotes/Program.cs
+++ netquotes/Program.cs
@@ -9,17 +9,15 @@ namespace netquotes
 
         static void Main(string[] args)
         {
+            UseLinqSpecsCore();
+        }
+
+        private static void UseLinqSpecsCore()
+        {
             CheckAndWriteCount("Spain");
             CheckAndWriteCount("France");
             CheckAndWriteCount("Portugal");
             CheckAndWriteCountList("Spain", "Portugal");
-        }
-
-        private static void CheckAndWriteCount(string country)
-        {
-            var customerFromCountrySpec = new CustomerFromCountrySpec(country);
-            var customers = _customerDao.Count(customerFromCountrySpec);
-            Console.WriteLine($"Total {country} = {customers}");
         }
 
         private static void CheckAndWriteCountList(string c1, string c2)
@@ -28,6 +26,13 @@ namespace netquotes
             var customerFromCountrySpecC2 = new CustomerFromCountrySpec(c2);
             var customers = _customerDao.Count(customerFromCountrySpecC1 || customerFromCountrySpecC2);
             Console.WriteLine($"Total {c1} and {c2} = {customers}");
+        }
+
+        private static void CheckAndWriteCount(string country)
+        {
+            var customerFromCountrySpec = new CustomerFromCountrySpec(country);
+            var customers = _customerDao.Count(customerFromCountrySpec);
+            Console.WriteLine($"Total {country} = {customers}");
         }
     }
 }
